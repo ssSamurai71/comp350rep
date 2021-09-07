ce97abce47a3eb7c1d8beae1ac2e77595ad9ee66
Arthur <66388988+ssSamurai71@users.noreply.github.com>
2021-09-06 22:51:39 -0700

    Create submitter_lambda_fuction.py
    This was made by me and the AWS team lead Jose Cahue. We worked on get the lambda function to send what the comment on line 8  to the back end.

---
+++ submitter_lambda_fuction.py
@@ -0,0 +1,14 @@
+import json
+
+def lambda_handler(event, context):
+    #printing event information out to cloudwatch to see what how the metadata looks
+    print("EVENT")
+    print(event)
+
+    #parse out the JSON from the metadata sent from the S3 bucket using python dictionary notation
+    reduceData = event['Records'][0]['s3']['object']['key']
+    
+    #send that data to the SQS where backend will parse it out
+    return {
+        'body': {'subdata': reduceData}
+    }
